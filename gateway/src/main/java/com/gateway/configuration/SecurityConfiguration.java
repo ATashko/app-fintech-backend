da3b8d921c03dfe013b26353cb756db8c539bc73
Tatiana García <86701915+Tatianag2@users.noreply.github.com>
2024-05-25 18:03:43 -0500

    add method for logout

--- gateway/src/main/java/com/gateway/configuration/SecurityConfiguration.java
+++ gateway/src/main/java/com/gateway/configuration/SecurityConfiguration.java
@@ -6,19 +6,40 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
-    @Bean
+	
+	private final ReactiveClientRegistrationRepository reactiveClientRegistrationRepository;
+	
+    public SecurityConfiguration(ReactiveClientRegistrationRepository reactiveClientRegistrationRepository) {
+		this.reactiveClientRegistrationRepository = reactiveClientRegistrationRepository;
+	}
+
+	@Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
                 .authorizeExchange(exchanges -> exchanges
                         .anyExchange().authenticated()
                 )
-                .oauth2Login(withDefaults());
+                .oauth2Login()
+                .and()
+                .logout()
+                .logoutSuccessHandler(oidcServerLogoutSuccessHandler());
+        
         return http.build();
     }
+	
+	private ServerLogoutSuccessHandler oidcServerLogoutSuccessHandler() {
+		OidcClientInitiatedServerLogoutSuccessHandler oidcClientInitiatedServerLogoutSuccesHandler
+		            = new OidcClientInitiatedServerLogoutSuccessHandler(reactiveClientRegistrationRepository);
+		oidcClientInitiatedServerLogoutSuccesHandler.setPostLogoutRedirectUri("http://localhost:9090/login");
+		return oidcClientInitiatedServerLogoutSuccesHandler;
+	}
 }
