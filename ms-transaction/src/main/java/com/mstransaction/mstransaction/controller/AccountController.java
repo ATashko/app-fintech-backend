3234b899f523baffd6464dd6e200c25f68fd4a48
Tatiana García <86701915+Tatianag2@users.noreply.github.com>
2024-06-10 11:48:33 -0500

    adding endpoint details account

--- ms-transaction/src/main/java/com/mstransaction/mstransaction/controller/AccountController.java
+++ ms-transaction/src/main/java/com/mstransaction/mstransaction/controller/AccountController.java
@@ -3,6 +3,8 @@ package com.mstransaction.mstransaction.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +28,9 @@ public class AccountController {
     public List<AccountDTO> getAccountsByUserId(@PathVariable String userId) {
         return accountService.getAccounts(userId);
     }
-
+	
+	@GetMapping("/{numberAccount}")
+    public AccountDTO getAccountDetail(@PathVariable String numberAccount, Authentication authentication) {
+        return accountService.getAccountDetail(numberAccount);
+    }
 }
