a6b0b0150e234124226b25327dc82ff191dfe857
Tatiana García <86701915+Tatianag2@users.noreply.github.com>
2024-06-11 23:39:12 -0500

    adding feign for converter
    Pending to resolve Eureka configuration, URL for connection is set.

---
+++ ms-transaction/src/main/java/com/mstransaction/mstransaction/client/IConverterClient.java
@@ -0,0 +1,12 @@
+package com.mstransaction.mstransaction.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "currencyconverter", url = "http://localhost:8083")
+public interface IConverterClient {
+	
+	@GetMapping("/convert")
+    public String convertCurrency(@RequestParam double amount, @RequestParam String from, @RequestParam String to);
+}
